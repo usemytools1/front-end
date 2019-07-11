6deddfbf641034a9c0cf255b06907e08c03cb487
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-07-11 02:12:16 -0400

    set up the skeleton JSX for the cards that will be used for the tools in the ToolList.js

--- use-my-tools/src/components/ToolComps/Tool.js
+++ use-my-tools/src/components/ToolComps/Tool.js
@@ -1,1 +1,24 @@
 // create the card for the tool
+import React from 'react'
+
+const tool = props => {
+  return (
+    <div className="toolCard">
+      <img className="toolImg" alt={props.tool.name} src={props.tool.img}></img>
+      <h3 className="toolName">{props.tool.name}</h3>
+      <span
+        className={`toolAvail ${props.tool.availability} ? available : notAvailable`}>
+        {props.tool.availability ? 'Available' : 'Unavailable'}
+      </span>
+      <p className="toolDesc">{props.tool.desc}</p>
+      <p className="toolPrice">${props.tool.price}</p>
+      {
+        props.tool.availability ?
+        <button className="canBeBorrowed">Borrow</button> :
+        <button className="cantBeBorrowed" disabled >Borrow</button>
+      }
+    </div>
+  )
+}
+
+export default tool
--- use-my-tools/src/components/ToolComps/ToolList.js
+++ use-my-tools/src/components/ToolComps/ToolList.js
@@ -1,1 +1,14 @@
 // Will iterate over the tools and map them
+import React, { Component } from 'react'
+
+export class ToolList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ToolList
