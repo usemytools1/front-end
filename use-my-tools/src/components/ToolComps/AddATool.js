26e23532336a0e36052637203dc04d79663a5535
chazr88 <chazr_88@hotmail.com>
2019-07-11 21:59:05 -0400

    Update AddATool.js
    Created boiler plate for add a tool

--- use-my-tools/src/components/ToolComps/AddATool.js
+++ use-my-tools/src/components/ToolComps/AddATool.js
@@ -1,1 +1,14 @@
 // Basic form to add a tool
+import React, { Component } from 'react'
+
+class AddATool extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default AddATool
