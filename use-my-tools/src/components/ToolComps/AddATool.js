d78b6f4d7138d01adaae77f717aaebae3cf32dcd
chazr88 <chazr_88@hotmail.com>
2019-07-11 23:13:15 -0400

    Update AddATool.js
    created most of the components. should be ready to test when we get some of the other commponents set up.

--- use-my-tools/src/components/ToolComps/AddATool.js
+++ use-my-tools/src/components/ToolComps/AddATool.js
@@ -1,14 +1,62 @@
-// Basic form to add a tool
-import React, { Component } from 'react'
+import React from "react";
 
-class AddATool extends Component {
+class AddATool extends React.Component {
+    state = {
+        tool: {
+            name: "",
+            brand: "",
+            category: "",
+            description: ""
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            tool: {
+                ...this.state.tool,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    handleAdd = e => {
+        e.preventDefault();
+        this.props.AddTool(this.state.tool);
+    };
+
     render() {
         return (
             <div>
-                
+                <form onSubmit={this.handleAdd}>
+                    <input
+                        type="text"
+                        name="name"
+                        value={this.state.tool.name}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="brand"
+                        value={this.state.tool.brand}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="category"
+                        value={this.state.tool.category}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="description"
+                        value={this.state.tool.description}
+                        onChange={this.handleChange}
+                    />
+                    <button>Add Tool</button>
+                </form>
             </div>
-        )
+        );
     }
 }
 
-export default AddATool
+export default AddATool;
