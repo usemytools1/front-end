12f8878c8cd8803429bd3cfd71ba1b4f9fbbf248
Dethrowe <dethrowe@pop-os.localdomain>
2019-07-17 20:29:31 -0400

    created this branch for editing the actions and reducers

--- use-my-tools/src/actions/index.js
+++ use-my-tools/src/actions/index.js
@@ -20,22 +20,22 @@ export const signUpAction = creds => dispatch => {
      .then(res => localStorage.setItem("token", res.data.payload));
 };
 
-// export const getTools = () => dispatch => {
-//     dispatch({ type: FETCH_DATA_START });
-//     axios
-//       .get("http://localhost:5000/api/tools", {
-//         headers: { Authorization: localStorage.getItem("token") }
-//       })
-//       .then(res => {
-//         console.log(res.data);
-//         dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data});
-//       })
-//       .catch(err => {
-//         console.log(err.response);
-//         if (err.response.status === 403) {
-//           localStorage.removeItem("token");
-//         }
-//         dispatch({ type: FETCH_DATA_FAILURE, payload: err.response });
-//       });
-//   };
+export const getTools = () => dispatch => {
+    dispatch({ type: FETCH_DATA_START });
+    axios
+      .get("https://use-my-tools-1.herokuapp.com/api/tools", {
+        headers: { Authorization: localStorage.getItem("token") }
+      })
+      .then(res => {
+        console.log(res.data);
+        dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data});
+      })
+      .catch(err => {
+        console.log(err.response);
+        if (err.response.status === 403) {
+          localStorage.removeItem("token");
+        }
+        dispatch({ type: FETCH_DATA_FAILURE, payload: err.response });
+      });
+  };
   
