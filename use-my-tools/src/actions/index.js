d7c72289c325b830254660f738d2e49ca9045fa7
Dethrowe <dethrowe@pop-os.localdomain>
2019-07-18 21:58:34 -0400

    added update and delete functionality

--- use-my-tools/src/actions/index.js
+++ use-my-tools/src/actions/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const LOGIN_START = "LOGIN_START";
 export const SIGN_UP_START = "SIGN_UP_START";
@@ -6,52 +6,87 @@ export const FETCH_DATA_START = "FETCH_DATA_START";
 export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
 export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE";
 
-export const ADD_TOOL = "ADD_TOOL"
-
+export const ADD_TOOL = "ADD_TOOL";
+export const DELETE_TOOL = "DELETE_TOOL";
+export const UPDATE_TOOL = "UPDATE_TOOL";
 
 export const loginAction = creds => dispatch => {
-    dispatch({ type: LOGIN_START });
-    return axios
-     .post("https://use-my-tools-1.herokuapp.com/api/auth/login", creds)
-     .then(res => localStorage.setItem("token", res.data.payload));
+  dispatch({ type: LOGIN_START });
+  return axios
+    .post("https://use-my-tools-1.herokuapp.com/api/auth/login", creds)
+    .then(res => localStorage.setItem("token", res.data.payload));
 };
 
 export const signUpAction = creds => dispatch => {
-    dispatch({ type: SIGN_UP_START });
-    return axios
-     .post("https://use-my-tools-1.herokuapp.com/api/auth/register", creds)
-     .then(res => localStorage.setItem("token", res.data.payload));
+  dispatch({ type: SIGN_UP_START });
+  return axios
+    .post("https://use-my-tools-1.herokuapp.com/api/auth/register", creds)
+    .then(res => localStorage.setItem("token", res.data.payload));
 };
 
 export const getTools = () => dispatch => {
-    dispatch({ type: FETCH_DATA_START });
-    axios
-      .get("http://localhost:3333/tools", {
-        headers: { Authorization: localStorage.getItem("token") }
-      })
-      .then(res => {
-        console.log(res.data);
-        dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data});
-      })
-      .catch(err => {
-        console.log(err.response);
-        if (err.response.status === 403) {
-          localStorage.removeItem("token");
-        }
-        dispatch({ type: FETCH_DATA_FAILURE, payload: err.response });
-      });
-  };
+  dispatch({ type: FETCH_DATA_START });
+  axios
+    .get("http://localhost:3333/tools", {
+      headers: { Authorization: localStorage.getItem("token") }
+    })
+    .then(res => {
+      console.log(res.data);
+      dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err.response);
+      if (err.response.status === 403) {
+        localStorage.removeItem("token");
+      }
+      dispatch({ type: FETCH_DATA_FAILURE, payload: err.response });
+    });
+};
 
 export const addTool = tool => dispatch => {
   dispatch({
     type: ADD_TOOL
-  })
+  });
   axios
-    .post('https://use-my-tools-1.herokuapp.com/api/tools', tool)
-    .then(res => dispatch({
-      type: ADD_TOOL,
-      payload: tool
-    }))
+    .post("https://use-my-tools-1.herokuapp.com/api/tools", tool)
+    .then(res =>
+      dispatch({
+        type: ADD_TOOL,
+        payload: tool
+      })
+    )
     .then(res => console.log("worked: ", res, tool))
-    .catch(err => console.log("fail: ", err))
-}
+    .catch(err => console.log("fail: ", err));
+};
+
+export const deleteTool = tool => dispatch => {
+  dispatch({
+    type: DELETE_TOOL
+  });
+  axios
+    .delete("https://use-my-tools-1.herokuapp.com/api/tools/:id", tool)
+    .then(res =>
+      dispatch({
+        type: DELETE_TOOL,
+        payload: tool
+      })
+    )
+    .then(res => console.log("worked: ", res, tool))
+    .catch(err => console.log("fail: ", err));
+};
+
+export const updateTool = tool => dispatch => {
+  dispatch({
+    type: UPDATE_TOOL
+  });
+  axios
+    .put("https://use-my-tools-1.herokuapp.com/api/tools/:id", tool)
+    .then(res =>
+      dispatch({
+        type: UPDATE_TOOL,
+        payload: tool
+      })
+    )
+    .then(res => console.log("worked: ", res, tool))
+    .catch(err => console.log("fail: ", err));
+};
--- use-my-tools/src/reducers/index.js
+++ use-my-tools/src/reducers/index.js
@@ -1,21 +1,48 @@
-import { LOGIN_START, FETCH_DATA_START, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, ADD_TOOL } from "../actions";
-
+import {
+  LOGIN_START,
+  FETCH_DATA_START,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAILURE,
+  ADD_TOOL,
+  UPDATE_TOOL,
+  DELETE_TOOL
+} from "../actions";
 
 const initialState = {
-  userOwnsTools: [],
-  userBorrowingTools: [],
+  userOwnsTools: [
+    {
+      id: 0,
+      owner: "",
+      availability: false,
+      name: "hammer",
+      img: "fake.img",
+      desc: "you hit things with this"
+    }
+  ],
+  userBorrowingTools: [
+    {
+      id: 5,
+      owner: "",
+      availability: false,
+      name: "blowtorch",
+      img: "fake.img",
+      desc: "you melt things with this"
+    }
+  ],
   fetchingTools: false,
-  tools: [{
-    owner: "",
-    availability: false,
-    name: "hammer", 
-    img: "fake.img",
-    desc: "you hit things with this"
-  }],
+  tools: [
+    {
+      id: 0,
+      owner: "",
+      availability: false,
+      name: "hammer",
+      img: "fake.img",
+      desc: "you hit things with this"
+    }
+  ],
   loggingIn: false,
   error: ""
 };
-
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -26,34 +53,44 @@ function reducer(state = initialState, action) {
       };
     }
     case FETCH_DATA_START: {
-        console.log("Start" )
-        return {
-            ...state,
-            error: "",
-            fetchingTools: true
-        };
+      console.log("Start");
+      return {
+        ...state,
+        error: "",
+        fetchingTools: true
+      };
     }
     case FETCH_DATA_SUCCESS: {
-        console.log("Fetched")
-        return {
-            ...state,
-            error: "",
-            fetchingTools: false,
-            tools: action.payload
-        };
+      console.log("Fetched");
+      return {
+        ...state,
+        error: "",
+        fetchingTools: false,
+        tools: action.payload
+      };
     }
     case FETCH_DATA_FAILURE: {
-        return {
-            ...state,
-            error: action.payload
-        }
+      return {
+        ...state,
+        error: action.payload
+      };
     }
     case ADD_TOOL:
       return {
         ...state,
         tools: [...state.tools, action.payload],
         addingTool: true
-      }
+      };
+    case UPDATE_TOOL:
+      return {
+        ...state,
+        tools: [...state.tools, action.payload],
+      };
+    case DELETE_TOOL:
+      return {
+        ...state,
+        tools: [...state.tools, action.payload],
+      };
     default:
       return state;
   }
